67af81cdbfbe4f58b235466bc9b8b6d0ca15e9e0
Domas <domas.bitvinskas@me.com>
2018-04-10 14:58:34 +0300

    Implement sensor/upload architecture

--- application/init.lua
+++ application/init.lua
@@ -1,103 +1,267 @@
---print(wifi.sta.getip())
---move everything to lc submodules
---log  datex5          XY.LL.MM.TT.HH
---ex   00.117.86400    XY.22.33.22.55
---       21 474 83648
+CASCADE_DEVICE_ID = 'cjfmlb3k40can0b825t0wzl5b'
+CASCADE_URL = 'http://cascade-backend.herokuapp.com'
+SENSOR_SCHEDULE = "* * * * *"
+UPLOAD_SCHEDULE = "* * * * *"
+DB_LOCATION = 'db.json'
 
-print(' chip:',node.chipid(),' heap:',node.heap())
+WIFI_SSID = "uabnamai"
+WIFI_PWD = "E7A0A3D980"
 
-wifi.eventmon.register(wifi.eventmon.STA_GOT_IP,
-function(T)
-     print("\n\tSTA_GOT_IP".."\n\tIP: "..T.IP.."\n\tMask: "..T.netmask.."\n\tGW "..T.gateway)
-     sntp.sync(nil, nil, nil, 1)
-     mytimer = tmr.create()
-     mytimer:register(20000, tmr.ALARM_SINGLE,
-          function()
-               dofile('box.lua')
+--WIFI_SSID = "Devyni Nol Astuoni"
+--WIFI_PWD = "iknowhowtotype908"
 
-                if pcall(box.sensor_temp) then
-                    print("Temp sent OK")
-                 else
-                    print("Temp err" )
-                 end
-                box.i2c_scan(1,2)
+JSON_HEADERS = 'Accept-Encoding: gzip, deflate, br\r\nContent-Type: application/json\r\nAccept: application/json\r\n'
 
-               --box.sensor_light()
-               box = nil
-          end)
-     --mytimer:register(20000, tmr.ALARM_SINGLE, function() print("ALARM 20k") end)
-     mytimer:start()
-     --out=ebox.home_ping(T.IP)
-end)
+sntp.sync(nil, nil, nil, 1)
 
-wifi.setmode(wifi.STATION)
---wifi.sta.config {ssid="Teo-55A397-Greitasis", pwd="CA5A698B5C"}
-wifi.sta.config {ssid="uabnamai", pwd="E7A0A3D980"}
-wifi.sta.connect()
-
--- in you init.lua:
-if adc.force_init_mode(adc.INIT_ADC)
-then
-  node.restart()
-  return -- don't bother continuing, the restart is scheduled
+ensureDb = function()
+    if file.exists(DB_LOCATION) then
+        print("DB exists")
+    else
+        print('DB doesnt exist. Creating...')
+        writeDb = file.open('db.json', 'w+')
+        writeDb:write(sjson.encode({ logs= {} }))
+        writeDb:close()
+    end
 end
---
 
+obtainWifi = function(cb)
+    print('Getting wifi...')
 
--- Serving static files
-dofile('httpServer.lua')
-httpServer:listen(80)
+    wifi.setmode(wifi.STATION)
+    wifi.sta.config({ ssid= WIFI_SSID, pwd = WIFI_PWD })
+    print('Configed.')
+    wifi.sta.connect(function()
+        print('Connecting...')
+        tmr.alarm(1, 1000, 1, function()
+            if wifi.sta.getip() == nil then
+                print("IP unavailable, Waiting...")
+            else
+                tmr.stop(1)
+                print('Connected!')
+                print("ESP8266 mode is: " .. wifi.getmode())
+                print("The module MAC address is: " .. wifi.ap.getmac())
+                print("Config done, IP is "..wifi.sta.getip())
+                cb()
+            end
+        end)
+    end)
+end
 
--- Custom API
--- Get text/html
-httpServer:use('/', function(req, res)
-	res:send('Hello ') -- /welcome?name=doge
-end)
+gql = function(body, cb)
+    print('GQL query to ' .. CASCADE_URL)
+    print(sjson.encode(body))
+    http.post(CASCADE_URL, JSON_HEADERS, sjson.encode(body), function(code, response)
+        if (code == -1) then
+            return print('gql error')
+        end
 
+        cb(sjson.decode(response))
+    end)
+end
 
-httpServer:use('/box/ping', function(req, res)
-     dofile('box.lua')
-     box.home_ping(T.IP)
-     box = nil
-     res:send('Hello ')
-end)
+getSensors = function(cb)
+    obtainWifi(function()
+        gql({
+          query = [[
+            query($deviceId: ID!) {
+                device(where: {
+                  id: $deviceId
+                }) {
+                    sensors {
+                        id
 
-httpServer:use('/box/light', function(req, res)
-     dofile('box.lua')
-     out=box.sensor_light()
-     box = nil
-	res:send(out)
-end)
+                        sensorType {
+                            name
+                        }
+                    }
+                }
+            }
+            ]],
+          variables = {
+            deviceId = CASCADE_DEVICE_ID
+          }
+        }, cb)
+    end)
+end
 
-httpServer:use('/box/temp', function(req, res)
-     --dofile('box.lua')
-     --out=box.sensor_temp()
-     --box = nil
-	--res:send(out)
-     res:send('Hello ')
-end)
+sensors = {}
 
-httpServer:use('/box/put', function(req, res)
-     --dofile('box.lua')
-     --box.sensor()
-     --box = nil
-	res:send('Hello ') -- /welcome?name=doge
-end)
+saveSensors = function(response)
+    print('Saving sensors...')
+    sensors = {}
+    for key, val in pairs(response.data.device.sensors) do
+        sensors[val.sensorType.name] = val.id
+    end
 
--- Get file
-httpServer:use('/box/log', function(req, res)
-	res:sendFile('local.log')
-end)
+    print('Sensors saved.')
+end
 
--- Get json
-httpServer:use('/json', function(req, res)
-	res:type('application/json')
-	res:send('{"doge": "smile"}')
-end)
+logs = {}
 
+UPLOAD_LOG_QUERY = [[
+  mutation($sensorId: ID!, $value: Float!) {
+    createLog(data: {
+      sensor: {
+        connect: {
+          id: $sensorId
+        },
+      },
+      value: $value,
+    }) {
+      id
+    }
+  }
+]]
 
-cron.schedule("*/10 * * * *", function(e)
-     --dofile('box.lua')
-     --box.sensor()
-     --box = nil
+insertLogIntoDb = function(log)
+    ensureDb()
+
+    readDb = file.open("db.json", "r")
+
+    originalJson = readDb:read()
+    readDb:close()
+    print(originalJson)
+    originalDb = sjson.decode(originalJson)
+
+    table.insert(originalDb.logs, log)
+
+    result = sjson.encode(originalDb)
+
+    writeDb = file.open('db.json', 'w+')
+    writeDb:write(result)
+    writeDb:close()
+
+    print('Inserted into DB, final value:', result)
+end
+
+removeLogFromDb = function(i)
+    ensureDb()
+
+    readDb = file.open("db.json", "r")
+
+    originalJson = readDb:read()
+    readDb:close()
+    print(originalJson)
+    originalDb = sjson.decode(originalJson)
+
+    table.remove(originalDb.logs, i)
+
+    result = sjson.encode(originalDb)
+
+    writeDb = file.open('db.json', 'w+')
+    writeDb:write(result)
+    writeDb:close()
+
+    print('Removed' .. i .. ' from DB, final value:', result)
+end
+
+dbLogs = function()
+    ensureDb()
+
+    readDb = file.open("db.json", "r")
+
+    originalJson = readDb:read()
+    readDb:close()
+    return sjson.decode(originalJson).logs
+end
+
+logSensor = function(sensorName, value)
+    if (not value) then
+        return
+    end
+
+    sensorId = sensors[sensorName]
+
+    insertLogIntoDb({
+        sensorId = sensorId,
+        value = value,
+    })
+end
+
+logHumidity = function()
+    humidity, temperature = bme280.humi()
+    logSensor('Humidity', humidity)
+end
+
+logTemperature = function()
+    humidity, temperature = bme280.humi()
+    logSensor('Temperature', temperature)
+end
+
+logPressure = function()
+    pressure, temperature = bme280.baro()
+    logSensor('Pressure', pressure)
+end
+
+initSensors= function()
+    alt, sda, scl =120, 1, 2                --altitude of the measurement place, GPIO2, GPIO0
+    i2c.setup(0, sda, scl, i2c.SLOW)        -- call i2c.setup() only once
+    print('Setting up bme280')
+    bme280.setup()
+    print('Bme280 set up')
+end
+
+logSensors = function()
+    for key, val in pairs(sensors) do
+      print('Logging ', key)
+
+      if key == 'Humidity' then
+        logHumidity()
+      elseif key == 'Temperature' then
+        logTemperature()
+      elseif key == 'Pressure' then
+        logPressure()
+      end
+
+      print('Logged ', key)
+    end
+end
+
+initLogging = function()
+    cron.schedule(SENSOR_SCHEDULE, function(e)
+      logSensors()
+      logHumidity()
+    end)
+end
+
+upload = function(log, cb)
+   obtainWifi(function()
+        gql({
+            query = UPLOAD_LOG_QUERY,
+            variables = log,
+        }, cb)
+    end)
+end
+
+uploadFirstLog = function()
+  first = dbLogs()[1]
+
+  if (not first) then
+    return print('No more logs to upload')
+  end
+
+  print('Unuploaded logs exist...')
+  upload(first, function(data)
+    removeLogFromDb(1)
+    print('Uploaded log', sjson.encode(data))
+    uploadFirstLog()
+  end)
+end
+
+initUploading = function()
+    cron.schedule(UPLOAD_SCHEDULE, function(e)
+        print('Starting to upload...')
+
+        uploadFirstLog()
+    end)
+end
+
+getSensors(function(data)
+    saveSensors(data)
+    initSensors()
+    print(sjson.encode(sensors))
+
+    initLogging()
+    initUploading()
 end)
