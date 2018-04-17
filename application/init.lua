3ca45e684bc70714a795afa2d657382e44d3370e
Domas <domas.bitvinskas@me.com>
2018-04-17 15:22:07 +0300

    Update id

--- application/init.lua
+++ application/init.lua
@@ -1,3 +1,7 @@
-CASCADE_DEVICE_ID = 'cjfwkfnkt96rx0b26nod5g5fm'
+-- Full Botanic
+-- CASCADE_DEVICE_ID = 'cjfwkfnkt96rx0b26nod5g5fm'
+
+-- Ieva
+CASCADE_DEVICE_ID = 'cjg3fvged77do0b267hqn20mk'
 
 dofile('runner.lc')
--- application/initUploading.lua
+++ application/initUploading.lua
@@ -1,5 +1,5 @@
 function initUploading()
-    tmr.alarm(2, 1000 * 60 * 15 + 20 * 1000, tmr.ALARM_AUTO, function()
+    tmr.alarm(2, 1000 * 35, tmr.ALARM_AUTO, function()
             print('Starting to upload...')
 
             require('uploadAll')
