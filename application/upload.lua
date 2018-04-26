7a84d3577a8f2cb17db57722e80ac68ad32533c4
Domas <domas.bitvinskas@me.com>
2018-04-26 15:35:56 +0300

    Add wifi logs

--- application/upload.lua
+++ application/upload.lua
@@ -5,6 +5,8 @@ function upload(log, cb)
         package.loaded['obtainWifi'] = nil
         obtainWifi = nil
         require('gql')
+
+        address, netmask, gateway = wifi.sta.getip()
         
         gql({
             query = [[
@@ -20,15 +22,46 @@ function upload(log, cb)
                 }) {
                   id
                 }
+
+                createWifiLog(
+                    device: {
+                      connect: {
+                        id: $deviceId
+                      },
+                    },
+                    hostname: $hostname,
+                    address: $address,
+                    netmask: $netmask,
+                    gateway: $gateway,
+                    mac: $mac,
+                    rssi: $rssi
+                ) {
+                }
               }
             ]],
-            variables = log,
+            variables = {
+                sensorId = log.sensorId,
+                value = log.value,
+                readingTime = log.readingTime,
+                deviceId = CASCADE_DEVICE_ID,
+                ssid = WIFI_SSID,
+                hostname = wifi.sta.gethostname(),
+                address = address,
+                netmask = netmask,
+                gateway = gateway,
+                mac = wifi.sta.getmac(),
+                rssi = wifi.sta.getrssi(),
+            },
         }, cb)
 
         package.loaded['gql'] = nil
         gql = nil
         cb = nil
         log = nil
+        
+        address = nil
+        netmask = nil
+        gateway = nil
     end)
 
     package.loaded['obtainWifi'] = nil
